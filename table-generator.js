62cb541bb25eb99848fe0d917cf0df0cb7f856f7
trsaman <31430122+trsaman@users.noreply.github.com>
2017-10-02 14:52:11 +0800

    Added action if condition is not met

--- table-generator.js
+++ table-generator.js
@@ -1,23 +1,30 @@
-var factor = prompt("Enter the largest value of the factors: ");
-var maxValue = parseInt(factor);
+var factor = prompt("Enter the largest value of the factors as a positive integer: ");
+var maxValue = parseFloat(factor);
 var row;
 var column;
 var multiplier;
+var check = Number.isInteger(maxValue);
 
-document.write("<h1><center>A multiplication table with "+maxValue+" as the greatest value of the factors</center></h1>");
+if (check == true && maxValue > 0) {
+	document.write("<h1><center>A multiplication table with "+maxValue+" as the greatest value of the factors</center></h1>");
 
-for (row = 0; row <= maxValue ; row++) {
-	document.write("<b>"+row+"\t");
-	if (row >= maxValue){
-		document.write("<br>");
-		for (column = 1; column <= maxValue ; column++){
-			document.write("<b>"+column+"\t</b>");
-			for (multiplier = 1; multiplier <= maxValue ; multiplier++){
-				document.write(column * multiplier+"\t");
-				if (multiplier >= maxValue){
-					document.write("<br>")
+	for (row = 0; row <= maxValue ; row++) {
+		document.write("<b>"+row+"\t");
+		if (row >= maxValue){
+			document.write("<br>");
+			for (column = 1; column <= maxValue ; column++){
+				document.write("<b>"+column+"\t</b>");
+				for (multiplier = 1; multiplier <= maxValue ; multiplier++){
+					document.write(column * multiplier+"\t");
+					if (multiplier >= maxValue){
+						document.write("<br>")
+					}
 				}
 			}
 		}
 	}
+}
+
+else {
+	document.write("Input is not a positive integer.  Reload page to try again.");
 }
